18d2ceb7a661109ddc1f6ab8163ae791d137d923
Gonzalo Ferreira <gonzqwe@gmail.com>
2022-09-29 18:44:41 -0300

    PÑ

--- js/init.js
+++ js/init.js
@@ -30,109 +30,103 @@ document.addEventListener("DOMContentLoaded", function (e) {
   getJSONData(apiURL).then(function (resultObj) {
     if (resultObj.status === "ok") {
       allmoviesarray = resultObj.data;
-      showmovies();
     }
   });
 });
 
-function movieInfo(movie){
+
+// Muestra los detalles de las pelis
+
+function movieInfo(movie) {
   let stars = "";
   let starsn = "";
   let generos = "";
   const release_date = new Date(movie.release_date);
   let year = release_date.getFullYear();
 
-    for (let s = 0; s < Math.round(movie.vote_average / 2); s++) {
-      stars += `
+  for (let s = 0; s < Math.round(movie.vote_average / 2); s++) {
+    stars += `
             <p class="fa fa-star checked"></p>
             `;
-    }
+  }
 
-    for (let n = Math.round(movie.vote_average / 2); n < 5; n++) {
-      starsn += `
+  for (let n = Math.round(movie.vote_average / 2); n < 5; n++) {
+    starsn += `
             <p class="fa fa-star"></p>
             `;
-    }
+  }
 
-    for (let g = 0; g < movie.genres.length; g++) {
-   //   console.log(movie.genres);
-      let genero = movie.genres[g];
-      generos += `
+  for (let g = 0; g < movie.genres.length; g++) {
+    let genero = movie.genres[g];
+    generos += `
       ${genero.name}
       `;
-    }
-    
-    return {
-     stars, starsn, generos, year, release_date
-    }
+  }
+
+  return {
+    stars,
+    starsn,
+    generos,
+    year,
+    release_date,
+  };
 }
 
 
+// evento para buscar y filtrar por el texto del input
 
+btn.addEventListener("click", function (e) {
+  let valorbusqueda = busq.value;
 
-busq.addEventListener("input", function (e) {
-  let valorbusqueda = e.target.value;
- // console.log(valorbusqueda);
-  // const movieExist = allmoviesarray.title.includes(valorbusqueda)
-  // const movieExist = allmoviesarray.title.includes(valorbusqueda)
-  const movieExist = console.log(allmoviesarray.title);
-
-
-  for (let i = 0; i < allmoviesarray.length; i++) {
-    const movie = allmoviesarray[i];
-    
-  
-  if  (valorbusqueda.includes(movie.title)){
- //   console.log("LAPELIES", movie.title);
-    console.table("Movie", movie);
-    cartelera(movie);
-  }
-
-  }
-
-
+  let pelisEncontradas = allmoviesarray.filter((movie) =>
+    movie.title.toLowerCase().includes(valorbusqueda.toLowerCase())
+  );
+  cartelera(pelisEncontradas);
 });
 
-function cartelera(movie){
-  
-  
-  const {stars, starsn, generos, year} = movieInfo(movie); 
 
-  const catalogo = `
-  <div data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop" id="movie" >
-  <h4>${movie.title}</h4>
-  <p>${movie.tagline}${stars + starsn}</p>
-  </div>
-  <div>
-  <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasTopLabel">${movie.title}</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    ${movie.overview}
-     <hr></hr> 
-    <p>${generos}</p>
-     </div>
-  <div class="dropdown" >
-    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">more</button>
-  <ul class="dropdown-menu >
-    <li><a class="dropdown-item">year:${year}</a></li>
-    <li><a class="dropdown-item">runtime:${movie.runtime}</a></li>
-    <li><a class="dropdown-item">budget:${movie.budget}</a></li>
-    <li><a class="dropdown-item">revenue:${movie.revenue}</a></li>
-  </ul>
-  </div>
-  </div>
-  </div>
-  `;
-      document.getElementById("lista").innerHTML = catalogo;
+// muestra las pelis en el html
+
+function cartelera(movies) {
+  let catalogo = "";
+
+  movies.forEach((movie) => {
+    const { stars, starsn, generos, year } = movieInfo(movie);
+
+    catalogo += `
+    <div data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop" id="movie" >
+    <h4>${movie.title}</h4>
+    <p>${movie.tagline}${stars + starsn}</p>
+    </div>
+    <div>
+    <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasTopLabel">${movie.title}</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      ${movie.overview}
+       <hr></hr> 
+      <p>${generos}</p>
+       </div>
+    <div class="dropdown" >
+      <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">more</button>
+    <ul class="dropdown-menu >
+      <li><a class="dropdown-item">year:${year}</a></li>
+      <li><a class="dropdown-item">runtime:${movie.runtime}</a></li>
+      <li><a class="dropdown-item">budget:${movie.budget}</a></li>
+      <li><a class="dropdown-item">revenue:${movie.revenue}</a></li>
+    </ul>
+    </div>
+    </div>
+    </div>
+    `;
+
+    console.log("loqtiene", catalogo);
+
+    document.getElementById("lista").innerHTML = catalogo;
+  });
 }
-
-
-
-
-
 
 //DISCARDED
 // let movie = moviesarray[i];
